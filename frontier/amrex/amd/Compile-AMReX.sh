84fb1124262dd1900dcc10bf3a1b5c6272201f3d
Liwei Ji <jiliwei.phys@gmail.com>
2024-06-18 20:12:39 -0400

    frontier: unload darshan-runtime in Load-Module-AMReX.sh

--- frontier/amrex/amd/Compile-AMReX.sh
+++ frontier/amrex/amd/Compile-AMReX.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "MPICH_GPU_SUPPORT_ENABLED    = $MPICH_GPU_SUPPORT_ENABLED"
+echo "PE_MPICH_GTL_DIR_amd_gfx90a  = $PE_MPICH_GTL_DIR_amd_gfx90a"
+echo "PE_MPICH_GTL_LIBS_amd_gfx90a = $PE_MPICH_GTL_LIBS_amd_gfx90a"
+
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_INSTALL_PREFIX=${HOME}/local/amrex24.06-rocm6.0-cce15-amd-gpumpi \
       -DCMAKE_PREFIX_PATH='/opt/rocm-6.0.0/lib/cmake/AMDDeviceLibs;/opt/rocm-6.0.0/lib/cmake/amd_comgr;/opt/rocm-6.0.0/lib/cmake/hip;/opt/rocm-6.0.0/lib/cmake/hiprand;/opt/rocm-6.0.0/lib/cmake/hsa-runtime64;/opt/rocm-6.0.0/lib/cmake/rocprim;/opt/rocm/lib/cmake/rocrand' \
--- frontier/amrex/amd/Load-Module-AMReX.sh
+++ frontier/amrex/amd/Load-Module-AMReX.sh
@@ -5,3 +5,5 @@ module load PrgEnv-cray
 module load cray-mpich/8.1.27
 module load craype-accel-amd-gfx90a
 module load amd-mixed/6.0.0
+
+module unload darshan-runtime
