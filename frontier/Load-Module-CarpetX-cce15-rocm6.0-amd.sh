e07db8915956b37399967e7135a07c3b26644c3e
Liwei Ji <jiliwei.phys@gmail.com>
2024-06-20 15:25:32 -0400

    frontier: update Load-Module-CarpetX files

--- frontier/Load-Module-CarpetX-cce15-rocm6.0-amd.sh
+++ frontier/Load-Module-CarpetX-cce15-rocm6.0-amd.sh
@@ -3,8 +3,8 @@
 ###############################################################################
 # basic
 ###############################################################################
-module reset
-module load PrgEnv-cray
+#module reset
+#module load PrgEnv-cray
 module load cray-mpich/8.1.27
 module load craype-accel-amd-gfx90a
 module load amd-mixed/6.0.0
