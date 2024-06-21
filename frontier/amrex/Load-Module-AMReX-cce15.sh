5521e06dd553c6edb7b6153b61a608494398b279
Liwei Ji <jiliwei.phys@gmail.com>
2024-06-21 12:05:07 -0400

    frontier: rename Load-Module-AMReX-cce15.sh

--- frontier/amrex/Load-Module-AMReX-cce15.sh
+++ frontier/amrex/Load-Module-AMReX-cce15.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-module reset
-module load PrgEnv-cray
+#module reset
+#module load PrgEnv-cray
 module load cray-mpich/8.1.27
 module load craype-accel-amd-gfx90a
 module load amd-mixed/6.0.0
