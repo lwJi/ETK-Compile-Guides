0abc51256191a6c2de6a9ded2fa4c9535899ed46
Liwei Ji <jiliwei.phys@gmail.com>
2024-09-25 13:04:43 -0400

    frontier: module load rocm

--- frontier/Load-Module-CarpetX.sh
+++ frontier/Load-Module-CarpetX.sh
@@ -8,6 +8,7 @@
 module load cray-mpich/8.1.28
 module load craype-accel-amd-gfx90a
 module load amd-mixed/6.1.3
+module load rocm/6.1.3
 
 ###############################################################################
 # more for carpetx
