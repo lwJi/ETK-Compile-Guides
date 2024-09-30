5414abf5bcac0bc914c9c91ad0b5c9627639d959
Liwei Ji <jiliwei.phys@gmail.com>
2024-09-30 13:52:45 -0400

    frontier: add cray

--- frontier/cray/Load-Module-CarpetX.sh
+++ frontier/cray/Load-Module-CarpetX.sh
@@ -4,22 +4,24 @@
 # basic
 ###############################################################################
 module reset
-module load PrgEnv-amd
-module load cpe/24.07
-module load amd/6.1.3
-module load rocm/6.1.3
+#module load cpe/24.07
+#module load PrgEnv-cray
 module load craype-accel-amd-gfx90a
+module load amd-mixed/6.1.3
+module load rocm/6.1.3
+
+module unload darshan-runtime
 
 ###############################################################################
 # more for carpetx
 ###############################################################################
+module load cray-fftw
 module load cray-hdf5-parallel
 module load openpmd-api
 module load boost
 module load hwloc
 module load adios2
-#module load libjpeg-turbo
-#module load openblas
-#module load zlib
-#module load gsl
-#module load cray-fftw
+module load libjpeg-turbo
+module load openblas
+module load zlib
+module load gsl
