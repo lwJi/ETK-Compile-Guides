edfa15f823863b4f6ac4835fd164d2e1a005ec49
Liwei Ji <jiliwei.phys@gmail.com>
2024-11-29 10:00:04 -0600

    vista: update vista.cfg

--- vista/Compile-ETK.sh
+++ vista/Compile-ETK.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd ${ETKPATH}/Cactus && \
+cd ${ETKPATH}/Cactus
+
+rm std*.log
+rm -r configs/etk
+
 gmake etk options=${ETKGUIDE}/vista.cfg && \
-cp ${ETKGUIDE}/../../ThornList/spacetimex.th configs/etk/ThornList && \
-gmake -j24 etk
+cp ${ETKGUIDE}/../ThornList/spacetimex.th configs/etk/ThornList && \
+gmake -j24 etk > >(tee stdout.log) 2> >(tee stderr.log)
