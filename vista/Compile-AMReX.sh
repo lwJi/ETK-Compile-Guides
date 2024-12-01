b6b5fe5795113c5cade46aadb48accb1114f891b
Liwei Ji <jiliwei.phys@gmail.com>
2024-11-30 22:01:18 -0600

    vista: update Compile-AMReX.sh

--- vista/Compile-AMReX.sh
+++ vista/Compile-AMReX.sh
@@ -12,6 +12,10 @@
 
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_INSTALL_PREFIX=${ETKPATH}/amrex-lib \
+      -DCMAKE_CUDA_COMPILER=nvcc \
+      -DCMAKE_CUDA_FLAGS='-pipe -g --compiler-options -march=native -std=c++17 --compiler-options -std=gnu++17 --expt-relaxed-constexpr --extended-lambda --gpu-architecture sm_86 --forward-unknown-to-host-compiler --Werror ext-lambda-captures-this --relocatable-device-code=true --objdir-as-tempdir -x cu' \
+      -DCMAKE_CUDA_COMPILER_FORCED=ON \
+      -DCMAKE_CUDA_COMPILE_FEATURES=cuda_std_17 \
       -DAMReX_GPU_BACKEND=CUDA \
       -DAMReX_CUDA_ARCH=8.6+PTX \
       -DAMReX_DIFFERENT_COMPILER=ON \
