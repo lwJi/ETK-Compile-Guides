f997656b91f54d10fa5e7c8bb0d0da154039f453
Liwei Ji <jiliwei.phys@gmail.com>
2024-11-30 20:35:23 -0600

    vista: update Export-AMReX.sh

--- vista/Export-AMReX.sh
+++ vista/Export-AMReX.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export AMREX_CUDA_ARCH=8.6+PTX
-export CC=$(which nvcc)
-export CXX=$(which nvcc)
-export FC=$(which nvfortran)
-export CFLAGS="-I${TACC_NVIDIA_INC}"
-export CXXFLAGS="-I${TACC_NVIDIA_INC}"
-export LDFLAGS="-L${TACC_NVIDIA_LIB}"
+export CC=$(which gcc)
+export CXX=$(which g++)
+export FC=$(which gfortran)
+export CFLAGS="-I${TACC_CUDA_INC}"
+export CXXFLAGS="-I${TACC_CUDA_INC}"
+export LDFLAGS="-L${TACC_CUDA_LIB}"
