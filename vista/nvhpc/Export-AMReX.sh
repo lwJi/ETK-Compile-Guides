f65558e6b83182d09329a4c61a9655bdee83290c
Liwei Ji <jiliwei.phys@gmail.com>
2024-12-01 19:58:45 -0600

    vista: update fils for nvhpc

--- vista/nvhpc/Export-AMReX.sh
+++ vista/nvhpc/Export-AMReX.sh
@@ -4,6 +4,6 @@ export AMREX_CUDA_ARCH=8.6+PTX
 export CC=$(which nvc)
 export CXX=$(which nvc++)
 export FC=$(which nvfortran)
-export CFLAGS="-I${TACC_NVIDIA_INC}"
-export CXXFLAGS="-I${TACC_NVIDIA_INC}"
-export LDFLAGS="-L${TACC_NVIDIA_LIB}"
+export CFLAGS="-I${TACC_NVIDIA_INC} -I${TACC_CUDA_INC}"
+export CXXFLAGS="-cuda -gpu=cc90,rdc -I${TACC_NVIDIA_INC} -I${TACC_CUDA_INC}"
+export LDFLAGS="-L${TACC_NVIDIA_LIB} -L${TACC_CUDA_LIB}"
