c8501625952fe541fd96eea975b8cc6097ef4d70
Liwei Ji <jiliwei.phys@gmail.com>
2024-12-01 21:19:38 -0600

    vista: use nvpl

--- vista/gcc/Load-Module-CarpetX.sh
+++ vista/gcc/Load-Module-CarpetX.sh
@@ -6,14 +6,15 @@ module unload openmpi
 module unload nvidia
 
 module load gcc/13.2.0
-module load cuda
-module load nvmath
-module load openmpi
+module load cuda/12.5
+module load nvmath/12.6.0
+module load nvpl/24.5
+module load openmpi/5.0.5
 module load phdf5
-module load adios2
 module load boost
 module load fftw3
 module load gsl
 module load zlib
 
+#module load adios2
 #module load silo
