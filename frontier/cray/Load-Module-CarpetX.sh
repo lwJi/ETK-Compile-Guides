c15c30ed07ef1a705066872af4c065a18f6a8b6a
Liwei Ji <jiliwei.phys@gmail.com>
2024-12-19 19:36:51 -0500

    frontier: update Load-Module-CarpetX.sh for cray

--- frontier/cray/Load-Module-CarpetX.sh
+++ frontier/cray/Load-Module-CarpetX.sh
@@ -5,12 +5,12 @@
 ###############################################################################
 #module reset
 
+module unload darshan-runtime
+
 module load cpe/24.07
 module load cray-mpich/8.1.30
 module load craype-accel-amd-gfx90a
 module load rocm/6.1.3
-
-module unload darshan-runtime
 
 ###############################################################################
 # more for carpetx
