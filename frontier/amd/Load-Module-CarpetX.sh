9e3ebc304d3aae3738187946ecade1f95fc6f403
Liwei Ji <jiliwei.phys@gmail.com>
2024-12-19 19:30:12 -0500

    frontier: add workaround for gpu-aware-mpi

--- frontier/amd/Load-Module-CarpetX.sh
+++ frontier/amd/Load-Module-CarpetX.sh
@@ -35,3 +35,10 @@ export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
 export MPICH_GPU_SUPPORT_ENABLED=1
 export PE_MPICH_GTL_DIR_amd_gfx90a="-L/opt/cray/pe/mpich/8.1.30/gtl/lib"
 export PE_MPICH_GTL_LIBS_amd_gfx90a="-lmpi_gtl_hsa"
+
+###############################################################################
+# workaround for gpu-aware-mpi
+###############################################################################
+export MPICH_GPU_IPC_CACHE_MAX_SIZE=1000  # for cray-mpich/8.1.30
+#export GTL_HSA_MAX_IPC_CACHE_SIZE=1000  # for cray-mpich/8.1.28 or cray-mpich/8.1.29
+
